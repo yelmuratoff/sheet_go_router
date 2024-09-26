bee5b5cdff3919346822805fae059ac587c94ed2
Yelaman Yelmuratov <e99xxx@gmail.com>
2024-09-26 20:53:29 +0500

    ! F more simple

--- lib/main.dart
+++ lib/main.dart
@@ -27,14 +27,9 @@ class NestedTabNavigationExampleApp extends StatelessWidget {
             routes: <RouteBase>[
               GoRoute(
                 path: '/a',
-                builder: (BuildContext context, GoRouterState state) =>
-                    const RootScreen(label: 'A', detailsPath: '/a/details'),
-                routes: <RouteBase>[
-                  GoRoute(
-                    path: 'details',
-                    builder: (BuildContext context, GoRouterState state) => const DetailsScreen(label: 'A'),
-                  ),
-                ],
+                builder: (BuildContext context, GoRouterState state) => const TabScreen(
+                  text: 'A',
+                ),
               ),
             ],
           ),
@@ -42,40 +37,9 @@ class NestedTabNavigationExampleApp extends StatelessWidget {
             routes: <RouteBase>[
               GoRoute(
                 path: '/b',
-                builder: (BuildContext context, GoRouterState state) => const RootScreen(
-                  label: 'B',
-                  detailsPath: '/b/details/1',
-                  secondDetailsPath: '/b/details/2',
+                builder: (BuildContext context, GoRouterState state) => const TabScreen(
+                  text: 'B',
                 ),
-                routes: <RouteBase>[
-                  GoRoute(
-                    path: 'details/:param',
-                    builder: (BuildContext context, GoRouterState state) => DetailsScreen(
-                      label: 'B',
-                      param: state.pathParameters['param'],
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-          StatefulShellBranch(
-            routes: <RouteBase>[
-              GoRoute(
-                path: '/c',
-                builder: (BuildContext context, GoRouterState state) => const RootScreen(
-                  label: 'C',
-                  detailsPath: '/c/details',
-                ),
-                routes: <RouteBase>[
-                  GoRoute(
-                    path: 'details',
-                    builder: (BuildContext context, GoRouterState state) => DetailsScreen(
-                      label: 'C',
-                      extra: state.extra,
-                    ),
-                  ),
-                ],
               ),
             ],
           ),
@@ -85,6 +49,26 @@ class NestedTabNavigationExampleApp extends StatelessWidget {
                 path: '/menu',
                 pageBuilder: (context, state) => const BottomSheetPage(
                   child: _MenuBottomSheetBody(),
+                ),
+              ),
+            ],
+          ),
+          StatefulShellBranch(
+            routes: <RouteBase>[
+              GoRoute(
+                path: '/c',
+                builder: (BuildContext context, GoRouterState state) => const TabScreen(
+                  text: 'C',
+                ),
+              ),
+            ],
+          ),
+          StatefulShellBranch(
+            routes: <RouteBase>[
+              GoRoute(
+                path: '/d',
+                builder: (BuildContext context, GoRouterState state) => const TabScreen(
+                  text: 'D',
                 ),
               ),
             ],
@@ -102,6 +86,27 @@ class NestedTabNavigationExampleApp extends StatelessWidget {
         ),
         routerConfig: _router,
       );
+}
+
+class TabScreen extends StatelessWidget {
+  const TabScreen({
+    required this.text,
+    super.key,
+  });
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          text,
+          style: const TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
 }
 
 class _MenuBottomSheetBody extends StatelessWidget {
@@ -148,7 +153,7 @@ class ScaffoldWithNavBar extends StatefulWidget {
 
 class _ScaffoldWithNavBarState extends State<ScaffoldWithNavBar> with TickerProviderStateMixin {
   bool _isSheetOpen = false;
-  late final TabController _tabController = TabController(length: 4, vsync: this);
+  late final TabController _tabController = TabController(length: 5, vsync: this);
 
   @override
   void dispose() {
@@ -165,10 +170,11 @@ class _ScaffoldWithNavBarState extends State<ScaffoldWithNavBar> with TickerProv
         controller: _tabController,
         indicator: TopIndicator(),
         tabs: const <Tab>[
-          Tab(icon: Icon(Icons.home), text: 'Section A'),
-          Tab(icon: Icon(Icons.work), text: 'Section B'),
-          Tab(icon: Icon(Icons.tab), text: 'Section C'),
+          Tab(icon: Icon(Icons.work), text: 'A'),
+          Tab(icon: Icon(Icons.work), text: 'B'),
           Tab(icon: Icon(Icons.menu), text: 'Menu'),
+          Tab(icon: Icon(Icons.tab), text: 'C'),
+          Tab(icon: Icon(Icons.settings), text: 'D'),
         ],
         onTap: (int index) async {
           await Future.delayed(const Duration(milliseconds: 200), () {
@@ -179,7 +185,7 @@ class _ScaffoldWithNavBarState extends State<ScaffoldWithNavBar> with TickerProv
               });
             }
           });
-          if (index == 3) {
+          if (index == 2) {
             setState(() {
               _isSheetOpen = true;
               GoRouter.of(context).push('/menu');
@@ -226,120 +232,6 @@ class _TopIndicatorBox extends BoxPainter {
       paint,
     );
   }
-}
-
-class RootScreen extends StatelessWidget {
-  const RootScreen({
-    required this.label,
-    required this.detailsPath,
-    this.secondDetailsPath,
-    super.key,
-  });
-
-  final String label;
-  final String detailsPath;
-  final String? secondDetailsPath;
-
-  @override
-  Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-          title: Text('Root of section $label'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              Text('Screen $label', style: Theme.of(context).textTheme.titleLarge),
-              const Padding(padding: EdgeInsets.all(4)),
-              TextButton(
-                onPressed: () {
-                  GoRouter.of(context).go(detailsPath, extra: '$label-XYZ');
-                },
-                child: const Text('View details'),
-              ),
-              const Padding(padding: EdgeInsets.all(4)),
-              if (secondDetailsPath != null)
-                TextButton(
-                  onPressed: () {
-                    GoRouter.of(context).go(secondDetailsPath!);
-                  },
-                  child: const Text('View more details'),
-                ),
-            ],
-          ),
-        ),
-      );
-}
-
-class DetailsScreen extends StatefulWidget {
-  const DetailsScreen({
-    required this.label,
-    this.param,
-    this.extra,
-    this.withScaffold = true,
-    super.key,
-  });
-
-  final String label;
-  final String? param;
-  final Object? extra;
-  final bool withScaffold;
-
-  @override
-  State<StatefulWidget> createState() => DetailsScreenState();
-}
-
-class DetailsScreenState extends State<DetailsScreen> {
-  int _counter = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    if (widget.withScaffold) {
-      return Scaffold(
-        appBar: AppBar(
-          title: Text('Details Screen - ${widget.label}'),
-        ),
-        body: _build(context),
-      );
-    } else {
-      return ColoredBox(
-        color: Theme.of(context).scaffoldBackgroundColor,
-        child: _build(context),
-      );
-    }
-  }
-
-  Widget _build(BuildContext context) => Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Text('Details for ${widget.label} - Counter: $_counter', style: Theme.of(context).textTheme.titleLarge),
-            const Padding(padding: EdgeInsets.all(4)),
-            TextButton(
-              onPressed: () {
-                setState(() {
-                  _counter++;
-                });
-              },
-              child: const Text('Increment counter'),
-            ),
-            const Padding(padding: EdgeInsets.all(8)),
-            if (widget.param != null)
-              Text('Parameter: ${widget.param!}', style: Theme.of(context).textTheme.titleMedium),
-            const Padding(padding: EdgeInsets.all(8)),
-            if (widget.extra != null) Text('Extra: ${widget.extra!}', style: Theme.of(context).textTheme.titleMedium),
-            if (!widget.withScaffold) ...<Widget>[
-              const Padding(padding: EdgeInsets.all(16)),
-              TextButton(
-                onPressed: () {
-                  GoRouter.of(context).pop();
-                },
-                child: const Text('< Back', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),
-              ),
-            ],
-          ],
-        ),
-      );
 }
 
 class BottomSheetPage extends Page {
